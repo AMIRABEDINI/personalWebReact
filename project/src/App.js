eac26e758669959abf17e9427bca8a40a05ebef3
Amir <amircjh2@gmail.com>
2019-09-07 02:39:20 +0430

    first commit

--- project/src/App.js
+++ project/src/App.js
@@ -2,7 +2,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
-<<<<<<< HEAD
+
 import Navbar from './components/navbar';
 class App extends Component {
   render() {
@@ -10,17 +10,6 @@ class App extends Component {
       <React.Fragment>
         <Navbar />
       </React.Fragment>
-=======
-
-class App extends  Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
->>>>>>> 35c48185c475832410dc5e7ab595df95a682cb23
-    )
-  }
+    )}
 }
-
 export default App;
