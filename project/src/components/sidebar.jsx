1572118615b66feccff5446cf0c08651423936c2
Amir <amircjh2@gmail.com>
2019-09-07 15:42:58 +0430

    first commit

--- project/src/components/sidebar.jsx
+++ project/src/components/sidebar.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
-
+import getNavLinks from '../services/navLinks';
 export default class SideBar extends Component {
+
     render() {
+         const navlinks = getNavLinks();
         return (
             <nav className='col-sm-3 col-md-3 col-lg-2 d-block  bg-light sidebar'>
                 <div className='sidebar-sticky'>
@@ -12,8 +14,20 @@ export default class SideBar extends Component {
                     </span>
                     <p>برنامه نویس و مهندس نرم افزار</p>
                     </div>
+                    <hr className='shadow'/>
+                    <ul className='nav flex-column'>
+                        {navlinks.map(nav => (
+                            <li className='nav-item' key={nav.id}>
+                                <a className='nav-link' href={nav.link}>
+                                    <span className={nav.icon}></span>
+                                    <span className='m-2'>{nav.text}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
             </nav>
         )
     }
 }
+ 
